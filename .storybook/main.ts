d0b82e64a5718c46071dc397cf7c51a8b1ad2047
KittichoteKamalapirat <kittichoteshane@gmail.com>
2023-12-10 20:53:43 +0700

    fix: add svgr loader for storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -17,5 +17,26 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  webpackFinal: async (config) => {
+    // add svgr loader (for Navbar's logo)
+    const imageRule = config.module?.rules?.find((rule) => {
+      const test = (rule as { test: RegExp }).test;
+
+      if (!test) {
+        return false;
+      }
+
+      return test.test(".svg");
+    }) as { [key: string]: any };
+
+    imageRule.exclude = /\.svg$/;
+
+    config.module?.rules?.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 export default config;
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,2 +1,10 @@
 export type ObjectKeys<T> = keyof T;
 export type ObjectValues<T> = T[keyof T];
+
+declare module "*.svg" {
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const content: string;
+
+  export { ReactComponent };
+  export default content;
+}
